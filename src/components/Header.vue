a3123c3be44fd8ced61d4ace975f260c95ac67aa
andreadldev <andrea.delisi2@gmail.com>
2022-11-24 19:15:34 +0100

    Aggiungi main + correzioni stile header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,61 +1,57 @@
-<script setup>
-</script>
-
 <template>
     <header>
         <nav class="container">
             <img src="../assets/img/dc-logo.png" alt="...">
             <div class="list-inline">
                 <ul>
-                <li> <a href="#">CHARACTERS</a> </li>
-                <li> <a href="#">COMICS</a> </li>
-                <li> <a href="#">MOVIES</a> </li>
-                <li> <a href="#">TV</a> </li>
-                <li> <a href="#">GAMES</a> </li>
-                <li> <a href="#">COLLECTIBLES</a> </li>
-                <li> <a href="#">VIDEOS</a> </li>
-                <li> <a href="#">FANS</a> </li>
-                <li> <a href="#">NEWS</a> </li>
-                <li> <a href="#">SHOP</a> </li>
-            </ul>
+                    <li> <a href="#">CHARACTERS</a> </li>
+                    <li> <a href="#">COMICS</a> </li>
+                    <li> <a href="#">MOVIES</a> </li>
+                    <li> <a href="#">TV</a> </li>
+                    <li> <a href="#">GAMES</a> </li>
+                    <li> <a href="#">COLLECTIBLES</a> </li>
+                    <li> <a href="#">VIDEOS</a> </li>
+                    <li> <a href="#">FANS</a> </li>
+                    <li> <a href="#">NEWS</a> </li>
+                    <li> <a href="#">SHOP</a> </li>
+                </ul>
             </div>
         </nav>
     </header>
 </template>
 
 <style lang="scss" scoped>
-@import "../assets/css/style.scss";
+    @import "../assets/css/style.scss";
 
-header {
-    background-color: white;
-    border-bottom: 1px solid black;
-    width: 100%;
-    padding: 10px 0;
-    position: relative;
-}
+    .container {
+        @include flex-inline;
+    }
 
-img {
-    width: 80px;
-}
+    header {
+        background-color: white;
+        border-bottom: 1px solid black;
+        padding: 10px 0;
+        position: relative;
+    }
 
-a {
-    @include a-custom(none, var(--custom-blue));
-    display: flex;
-    justify-content: center;
-}
+    img {
+        width: 80px;
+    }
 
-a:hover:before {
-    content: "";
-    display: block;
-    position: absolute;
-    background-color: var(--custom-blue);
-    width: 35px;
-    height: 3px;
-    bottom: 0;
-    margin: auto;
-}
+    a {
+        @include a-custom(none, var(--custom-blue));
+        display: flex;
+        justify-content: center;
+    }
 
-.container {
-    @include flex-inline;
-}
+    a:hover:before {
+        content: "";
+        display: block;
+        position: absolute;
+        background-color: var(--custom-blue);
+        width: 35px;
+        height: 3px;
+        bottom: 0;
+        margin: auto;
+    }
 </style>
---
+++ src/components/Main.vue
@@ -0,0 +1,70 @@
+<template>
+    <main>
+        <section class="section-top">
+            <div class="container">
+                -------
+            </div>
+        </section>
+
+        <section class="section-bottom">
+            <div class="container list-inline">
+                <ul>
+                    <li>
+                        <a href="#"><img src="../assets/img/buy-comics-digital-comics.png" alt="..."></a>
+                        <a href="#">DIGITAL COMICS</a>
+                    </li>
+                    <li>
+                        <a href="#"><img href="#" src="../assets/img/buy-comics-merchandise.png" alt="..."></a>
+                        <a href="#">DC MERCHANDISE</a>
+                    </li>
+                    <li>
+                        <a href="#"><img src="../assets/img/buy-comics-subscriptions.png" alt="..."></a>
+                        <a href="#">SUBSCRIPTION</a>
+                    </li>
+                    <li>
+                        <a href="#"><img src="../assets/img/buy-comics-shop-locator.png" alt="..."></a>
+                        <a href="#">COMIC SHOP LOCATOR</a>
+                    </li>
+                    <li>
+                        <a href="#"><img src="../assets/img/buy-dc-power-visa.svg" alt="..."></a>
+                        <a href="#">DC POWER VISA</a>
+                    </li>
+                </ul>
+            </div>
+        </section>
+    </main>
+</template>
+
+<style lang="scss" scoped>
+    @import "../assets/css/style.scss";
+
+    .section-top {
+        background-color: var(--custom-black);
+        padding: 40px 0;
+    }
+
+    .section-bottom {
+        background-color: var(--custom-blue);
+        padding: 40px 0;
+    }
+
+    a {
+        @include a-custom(underline, inherit);
+        display: flex;
+        justify-content: center;
+        padding: 0 5px;
+    }
+
+    .list-inline {
+        ul {
+            justify-content: space-between;
+        }
+        li {
+            @include flex-inline;
+        }
+
+        img {
+            width: 50px;
+        }
+    }
+</style>
