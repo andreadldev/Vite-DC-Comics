06844a0e69219684eee3d243eea8e06e5dd9c63d
andreadldev <andrea.delisi2@gmail.com>
2022-11-26 18:56:35 +0100

    Sezione centrale comics

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,8 +1,25 @@
+<script>
+    import json from '../data/dc-comics.json' assert {type: 'json'};
+    export default {
+        data() {
+            return {
+                comicsList: json
+            }
+        }
+    }
+</script>
+
 <template>
     <main>
+        <section class="jumbotron"></section>
         <section class="section-top">
             <div class="container">
-            CONTENT
+                <div class="card" v-for="comic in comicsList">
+                    <div class="img-container">
+                        <img :src=comic.thumb :alt=comic.series>
+                    </div>
+                    <span>{{comic.series}}</span>
+                </div>
             </div>
         </section>
 
@@ -38,10 +55,55 @@
 <style lang="scss" scoped>
 @import "../assets/css/style.scss";
 
+.jumbotron {
+        background-image: url('../assets/img/jumbotron.jpg');
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-position: 50% 0;
+        height: 24rem;
+        width: 100%;
+    }
+
 .section-top {
     min-height: calc(100% - 9.625rem);
     background-color: var(--custom-black);
-    padding: 2.5rem 0;
+    padding: 3rem 0;
+    position: relative;
+    
+    .container {
+        margin-bottom: -50px;
+        display: flex;
+        flex-wrap: wrap;
+    }
+
+    .card {
+        width: calc((100% / 6) - 20px);
+        margin: 0 10px;
+        margin-bottom: 50px;
+        cursor: pointer;
+        .img-container {
+            overflow: hidden;
+        }
+
+        img{
+            width: 100%;
+            margin-bottom: -50%;
+        }
+        span {
+            display: block;
+            margin-top: 5px;
+        }
+    }
+}
+
+.section-top::before {
+    content: 'CURRENT SERIES';
+    display: block;
+    background-color: var(--custom-blue);
+    position: absolute;
+    padding: 7px 15px;
+    top: -18px;
+    left: 15%;
 }
 
 .section-bottom {
--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import { createApp } from 'vue'
 import './assets/css/style.scss'
 import App from './App.vue'
-import comicsList from './dc-comics.json' assert {type: 'json'}
 
 createApp(App).mount('#app')
