d4374da4f7debc913794be2329bb086dd1be63db
andreadldev <andrea.delisi2@gmail.com>
2022-11-27 18:13:14 +0100

    Generazione dinamica voci nav

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,19 +1,20 @@
+<script>
+    export default {
+        data() {
+            return {
+                menuItems: ['Characters', 'Comics', 'Movies', 'TV', 'Games', 'Collectibles', 'Videos', 'Fans', 'News', 'Shop']
+            }
+        }
+    }
+</script>
+
 <template>
     <header>
         <nav class="container">
             <img src="../assets/img/dc-logo.png" alt="..." />
             <div class="list-inline">
                 <ul>
-                <li><a href="#">CHARACTERS</a></li>
-                <li><a href="#">COMICS</a></li>
-                <li><a href="#">MOVIES</a></li>
-                <li><a href="#">TV</a></li>
-                <li><a href="#">GAMES</a></li>
-                <li><a href="#">COLLECTIBLES</a></li>
-                <li><a href="#">VIDEOS</a></li>
-                <li><a href="#">FANS</a></li>
-                <li><a href="#">NEWS</a></li>
-                <li><a href="#">SHOP</a></li>
+                    <li v-for="(item, index) in menuItems"><a href="#">{{menuItems[index]}}</a></li>
                 </ul>
             </div>
         </nav>
